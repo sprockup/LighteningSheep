2b620a210848a4bbcee9116c243172ce02b28d47
sprockup <sprockup@users.noreply.github.com>
2014-11-06 18:32:04 -0500

    Initial project import

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public GUISkin skin;
+
+	// Use this for GUI elements
+	void OnGUI()
+	{
+		// Apply skin
+		GUI.skin = skin;
+
+		// Dynamic width & height based on screen size
+		float boxHeight, boxWidth;
+		boxWidth = Screen.width;
+		boxHeight = Screen.height * .4f;
+		// Create rect on top of menu
+		Rect winScreenRect = new Rect(10, 
+		                              10, boxWidth, boxHeight);
+		GUI.Label(winScreenRect, "Lightening Sheep!");
+		
+		if (GUI.Button(new Rect(10,150,300,80), "Play"))
+		{
+			// Play Clicked
+			Application.LoadLevel(1);
+		}
+		if (GUI.Button(new Rect(10,240,300,80), "Quit"))
+		{
+			// Quit Clicked
+			Application.Quit();
+		}
+	}
+}
