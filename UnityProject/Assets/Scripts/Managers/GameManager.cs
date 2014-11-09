b865627273d82dd0566fe3918464b6f2702fe0e0
sprockup <sprockup@users.noreply.github.com>
2014-11-08 21:39:34 -0500

    Adding new folder structure and unity project

---
+++ UnityProject/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,134 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	private int currentScore;
+	private int highScore;
+
+	private int currentLevel = 0;
+	private int unlockedLevel;
+
+	public GUISkin skin;
+	public Rect timerRect;
+	public Rect sheepCountRect;
+	public float startTime = 60.0f;
+	private string currentTime;
+	
+	private bool showWinScreen = false;
+	private int sheepCount;
+	
+	public ScoreManager scoreManager;
+	public GameObject spawnLocation;
+	
+	void Start()
+	{
+
+		scoreManager = new ScoreManager();
+		/**
+		totalTokenCount = tokenParent.transform.childCount;
+		currentLevel = PlayerPrefs.GetInt("Level Completed");
+		if (PlayerPrefs.GetInt("Level Completed") <= 0)
+		{
+			currentLevel = 0;
+		}
+		*/
+		
+		// Start function to spawn sheep
+		//StartCoroutine(SpawnSheep());
+	}
+
+	void Update()
+	{
+		if (!showWinScreen)
+		{
+			startTime -= Time.deltaTime;
+			currentTime = string.Format("{0:0.00}", startTime);
+		}
+						
+		// Count the sheep
+		GameObject[] sheep = GameObject.FindGameObjectsWithTag("Sheep");
+		sheepCount = sheep.GetLength(0);
+		
+		
+		if (startTime <= 0 || sheepCount <= 0)
+		{
+			CompleteLevel();
+		}
+		
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			Application.LoadLevel("main_menu"); 
+		}
+	}
+
+	void OnGUI()
+	{
+		GUI.skin = skin;
+		GUI.Label(timerRect, currentTime, skin.GetStyle("Timer"));
+		GUI.Label(sheepCountRect, sheepCount.ToString(), skin.GetStyle("SheepCount"));
+		
+		if (showWinScreen)
+		{
+			// Dynamic width & height based on screen size
+			float boxHeight, boxWidth;
+			boxWidth = Screen.width * .8f;
+			boxHeight = Screen.height * .4f;
+			// Create rect on top of level
+			Rect winScreenRect = new Rect((Screen.width/2) - (boxHeight/2), 
+			                              (Screen.height/2) - (boxHeight/2), 
+			                              boxWidth, boxHeight);
+			GUI.Box(winScreenRect, "Level Completed!");
+			
+			if (GUI.Button(new Rect(winScreenRect.x + winScreenRect.width - 170, 
+			                    winScreenRect.y + winScreenRect.height - 50,
+			                    150, 40), "Continue"))
+            {
+            	// Continue clicked
+				showWinScreen = false;
+				Application.LoadLevel("main_menu");
+            }
+            
+			if (GUI.Button(new Rect(winScreenRect.x + 10, 
+			                        winScreenRect.y + winScreenRect.height - 50,
+			                        100, 40), "Quit"))
+			{
+				// Quit clicked
+				showWinScreen = false;
+				Application.LoadLevel("main_menu");
+			}
+            
+            GUI.Label(new Rect(winScreenRect.x + 20, winScreenRect.y + 40, 300, 50), startTime.ToString());
+            
+		}
+	}
+
+	public void CompleteLevel()
+	{
+		showWinScreen = true;
+	}
+	
+	void LoadNextLevel()
+	{
+		currentLevel = 0;
+		Application.LoadLevel(currentLevel);
+	}
+	
+	void SaveGame()
+	{
+		
+	}
+	
+	IEnumerator SpawnSheep()
+	{
+		bool spawning = true;
+		
+		while (spawning)
+		{
+			GameObject barn = GameObject.Find("Barn");
+			//barn.
+			// Wait for that amount of time
+			yield return new WaitForSeconds(30f);
+		}
+	}
+}
---
+++ UnityProject/Assets/Scripts/Managers/ScoreManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class ScoreManager {
+
+	private int currentScore;
+	private int highScore;
+
+	// Use this for initialization
+	public ScoreManager()
+	{
+		currentScore = 0;
+		highScore = 0;
+	}
+
+	int GetScore()
+	{
+		return currentScore;
+	}
+	
+	int AddToScore(int amountToAdd)
+	{
+		currentScore += amountToAdd;
+		return currentScore;
+	}
+}
