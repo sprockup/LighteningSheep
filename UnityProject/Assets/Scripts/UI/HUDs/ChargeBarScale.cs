6a9495f88caa2c2ae6937d739ed2f8ba0e53ec9b
sprockup <sprockup@users.noreply.github.com>
2014-11-26 14:57:28 -0500

    Adding HUD scaling scripts, new Atlas, texture and prefab

---
+++ UnityProject/Assets/Scripts/UI/HUDs/ChargeBarScale.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChargeBarScale : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		this.transform.localScale = new Vector3(Screen.width - 160, 1);
+		//this.transform.position = new Vector3(Screen.width / 2, 5);
+	}
+}
